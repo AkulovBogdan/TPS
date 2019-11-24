b09d2989f1c554c930fb4669e690752cffb38b30
NYehor <yehornedbalo@gmail.com>
2019-11-24 16:01:33 +0200

    initial commit

---
+++ TPSForm/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPSForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Method();
+        }
+
+        public void Method()
+        {
+            Model model = new Model();
+
+            double tmp = model.GetResultOfModel(FuelType.Coal, 86400, 0.35, 0.35);
+            tmp = tmp / 1_000_000;
+        }
+    }
+}
---
+++ TPSForm/Model.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPSForm
+{
+    enum FuelType
+    {
+        Lignite,
+        Anthracite,
+        Coal
+    }
+    class Model
+    {
+        public const double MassOfFuelPerSecond = 1;
+
+        Dictionary<FuelType, double> coefFuel = new Dictionary<FuelType, double>()
+        {
+            { FuelType.Lignite, 10500.0 * 1000},
+            { FuelType.Anthracite, 26800.0 * 1000},
+            { FuelType.Coal, 20900.0 * 1000}
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns>kg</returns>
+        private double GetMassOfFuel(int seconds)
+        {
+            return MassOfFuelPerSecond * seconds;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="specificHeat"></param>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public double GetEnergyFromFuel(double specificHeat, int seconds)
+        {
+            return GetMassOfFuel(seconds) * specificHeat;
+        }
+
+        public double GetEnergyFromSteam(double energy, double ccd)
+        {
+            return energy * ccd;
+        }
+
+        public double GetEnergy(double energy, double ccd)
+        {
+            return energy * ccd;
+        }
+
+        public double GetResultOfModel(FuelType fuel, int deltaTimeSeconds, double ccdTurbin, double ccdSteam)
+        {
+            double coef;
+            coefFuel.TryGetValue(fuel, out coef);
+
+            return GetEnergy(GetEnergyFromSteam(GetEnergyFromFuel(coef, deltaTimeSeconds),ccdSteam),ccdTurbin);
+        }
+    }
+}
